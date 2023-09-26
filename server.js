df5c4db7ef64645cd79b1fbe771fd167e342a22f
Jeffruiz1502003 <123280099+Jeffruiz1502003@users.noreply.github.com>
2023-09-26 17:19:07 -0400

    adding changes

--- server.js
+++ server.js
@@ -33,6 +33,6 @@ app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) }
 app.put('/image', (req, res) => { image.handleImage(req, res, db) })
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res, db) })
 
-app.listen(3000, ()=> {
-	console.log('app is running on port 3000');
-})
+// app.listen(3000, ()=> {
+// 	console.log('app is running on port 3000');
+// })
