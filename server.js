cde54bf58f1d200dc33f230854f4f6104f265aa8
Jeffruiz1502003 <123280099+Jeffruiz1502003@users.noreply.github.com>
2023-09-29 19:31:39 -0400

    adding changes to database

--- server.js
+++ server.js
@@ -13,12 +13,12 @@ const db = knex({
   client: 'pg',
   connection: {
     connectionString : process.env.DATABASE_URL,
-    ssl: { rejectedUnauthorized: false },
-    host : process.env.DATABASE_HOST,
-    port : 5432,
-    user : process.env.DATABASE_USER,
-    password : process.env.DATABASE_PW,
-    database : process.env.DATABASE_DB
+    ssl: true,
+    // host : process.env.DATABASE_HOST,
+    // port : 5432,
+    // user : process.env.DATABASE_USER,
+    // password : process.env.DATABASE_PW,
+    // database : process.env.DATABASE_DB
   }
 });
 
