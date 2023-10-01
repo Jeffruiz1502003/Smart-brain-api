6a2fd40e3cde370cbf9400eef1007947736e81b9
Jeffruiz1502003 <123280099+Jeffruiz1502003@users.noreply.github.com>
2023-10-01 16:26:20 -0400

    Adding changes to the server

--- server.js
+++ server.js
@@ -9,16 +9,39 @@ const signin  = require('./Controllers/signin');
 const profile = require('./Controllers/profile');
 const image = require('./Controllers/image');
 
+
+// // const db = knex({
+// //   client: 'pg',
+// //   connection: {
+// //     host : '127.0.0.1',
+// //     port : 5432,
+// //     user : 'postgres',
+// //     password : 'Js',
+// //     database : 'smart-brain'
+// //   }
+// });
+// const db = knex({
+//   client: 'pg',
+//   connection: {
+//     connectionString: 'postgres://mydb_ptsw_user:jvlX4QMMrgHDc5UcaonSZHmgE6HYad5M@dpg-ck8svinq54js73e8h8b0-a/mydb_ptsw',
+//     // ssl: { rejectedUnauthorized: false },
+//     host: 'dpg-ck8svinq54js73e8h8b0-a',
+//     port: 5432,
+//     user: 'mydb_ptsw_user',
+//     password: 'jvlX4QMMrgHDc5UcaonSZHmgE6HYad5M',
+//     database: 'mydb_ptsw': 
+//   }
+// });
 const db = knex({
   client: 'pg',
   connection: {
     connectionString : process.env.DATABASE_URL,
     ssl: { rejectedUnauthorized: false },
-    host : process.env.DATABASE_HOST,
-    port : 5432,
-    user : process.env.DATABASE_USER,
-    password : process.env.DATABASE_PW,
-    database : process.env.DATABASE_DB
+    host: process.env.DATABASE_HOST,
+    port: 5432,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PW,
+    database: process.env.DATABASE_DB
   }
 });
 
@@ -26,7 +49,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', (req, res) => {res.send('Success') });
+app.get('/', (req, res) => { res.send('Success') });
 
 app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt)});
 
